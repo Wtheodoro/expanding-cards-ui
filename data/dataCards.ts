790a60120aae7236eff3a571b30ce484a1770723
Walison Theodoro <theodoro.walison@gmail.com>
2022-11-14 11:45:41 -0300

    config: init next and add styled components

---
+++ data/dataCards.ts
@@ -0,0 +1,25 @@
+const DATA_CARDS = [
+  {
+    id: 1,
+    imageUrl: '/humans/3.webp',
+    label: 'Writing',
+    name: 'Writing Course',
+    topicList: 100,
+  },
+  {
+    id: 2,
+    imageUrl: '/humans/2.webp',
+    label: 'Coding',
+    name: 'Coding Course',
+    topicList: 120,
+  },
+  {
+    id: 3,
+    imageUrl: '/humans/1.webp',
+    label: 'Business',
+    name: 'Business Course',
+    topicList: 150,
+  },
+]
+
+export default DATA_CARDS
